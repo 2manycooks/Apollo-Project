b0c85d97bc8e3571da813cc17298798ab440caf1
2manycooks <73556731+2manycooks@users.noreply.github.com>
2023-11-14 19:30:30 -0800

    apollo server ready

---
+++ server/schema.js
@@ -0,0 +1,49 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+
+    type Query {
+        quakes: [Quake]!
+        quake(id: ID!): Quake
+
+        me: User
+    }
+
+    type Quake {
+        id: ID!
+        location: String
+        magnitude: Float
+        when: String
+        time: String
+    }
+
+    type User {
+        id: ID!
+        username: String!
+        email: String!
+        password: String!
+        records: [Quake]!
+    }
+
+    type Mutation {
+        # if false, saving record failled -- check errors
+        saveRecord(recordId: ID!): RecordUpdateResponse!
+
+        # if false, removing record failed -- check errors
+        deleteRecord(recordId: ID!): RecordUpdateResponse!
+
+        # login token
+        login(email: String!, password: String!): String
+
+    }
+
+    type RecordUpdateResponse {
+        success: Boolean!
+        message: String
+        records: [Quake]
+    }
+`;
+
+
+
+module.exports = typeDefs;
