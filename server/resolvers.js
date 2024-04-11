39bce70c06f9a60c81cb0bf0f6cd942fa9cdef65
2manycooks <73556731+2manycooks@users.noreply.github.com>
2024-04-11 13:17:36 -0700

    removed object brackets on resolvers, was creating issue with user query

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,14 +1,8 @@
 module.exports = {
     Query : {
-        quakes: (_, __, { dataSources }) => {
-            return dataSources.quakeAPI.getAllQuakes()
-        },
-        quake: (_, { id }, { dataSources }) => {
-            dataSources.quakeAPI.getQuakeById({ quakeId : id })
-        },
-        users: (_, __, { dataSources }) => {
-            dataSources.userAPI.getUsers()
-        }
+        quakes: (_, __, { dataSources }) => dataSources.quakeAPI.getAllQuakes(),
+        quake: (_, { id }, { dataSources }) => dataSources.quakeAPI.getQuakeById({ quakeId : id }),
+        users: (_, __, { dataSources }) => dataSources.userAPI.getUsers()
         // QUERY USER me: (_, __, { dataSources }) => dataSources.userAPI.finderOrCreateUser()
     }
 }
